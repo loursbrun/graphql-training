657f78d17d228957c3ff7d686028aeacfbb80db3
fbrun <fabien.brun@kcitylabs.fr>
2019-03-27 14:42:35 +0100

    Add React component for movie list

---
+++ graphql-react/client/components/movie-list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+    render() {
+        return (
+            <div>
+                Liste de film
+            </div>
+        );
+    }
+}
+
+export default MovieList;
