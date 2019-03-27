c1244c2f720eef220b247ff3a4ac4f26a531767b
fbrun <fabien.brun@kcitylabs.fr>
2019-03-27 11:33:07 +0100

    Add React and GraphQL example

---
+++ graphql-react/server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Remplacer avec ton url mlab
+// mongodb+srv://fabien:<password>@cluster0-h1ulm.mongodb.net/test?retryWrites=true
+// mongodb://robinuser:robinuser@ds245347.mlab.com:45347/dbrobin001
+const MONGO_URI = 'mongodb://fabien:fabien@cluster0-h1ulm.mongodb.net';
+if (!MONGO_URI) {
+  throw new Error('Tu dois fournir une url mongoDB');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, 
+{
+  useMongoClient:true
+});
+mongoose.connection
+    .once('open', () => console.log('Connecté à MongoLab'))
+    .on('error', error => console.log('Erreur de connexion à MongoLab:', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true,
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
