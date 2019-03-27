605a2e5cc5a1ec86ddbbd59b23f20c90205f2465
fbrun <fabien.brun@kcitylabs.fr>
2019-03-27 15:30:23 +0100

    Add rooting for react app

---
+++ graphql-react/client/components/movie-create.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieCreate extends Component {
+    render() {
+        return (
+            <div>
+                Movie Create
+            </div>
+        );
+    }
+}
+
+export default MovieCreate;
--- graphql-react/client/index.js
+++ graphql-react/client/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
-import MovieList from "./components/movie-list"
+import MovieList from "./components/movie-list";
+import MovieCreate from "./components/movie-create";
+import { Router, Route, hashHistory, IndexRedirect } from "react-router";
 
-const client = new ApolloClient ({});
+const client = new ApolloClient({});
 
 const Root = () => {
   return (
-  <ApolloProvider client={client}>
-    <MovieList/>
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/">
+          <IndexRedirect to="/movies"/>
+          <Route path="/movies" component={MovieList}/>
+          <Route path="/movies/create" component={MovieCreate}/>
+        </Route>
+      </Router>
+    </ApolloProvider>
   )
 };
 
